9c82198ba4c78efc4781347659f8b129bd24b47f
jeongjeong2 <wjdgus01227@gmail.com>
2023-05-03 01:10:59 +0900

    velog정리

---
+++ 대학생 기본반 문제집/8979.올림픽.py
@@ -0,0 +1,16 @@
+"""
+https://www.acmicpc.net/problem/8979
+"""
+N, K = list(map(int,input().split()))
+country = []
+for i in range(N):
+    medal = list(map(int,input().split()))
+    country.append(medal)
+country.sort(key = lambda x : (-x[1],-x[2],-x[3])) # 나열 방법 다시 확인할 것
+for i in range(N):
+    if country[i][0] == K:
+        index = i
+for i in range(N):
+    if country[index][1:] == country[i][1:]:
+        print(i + 1)
+        break
