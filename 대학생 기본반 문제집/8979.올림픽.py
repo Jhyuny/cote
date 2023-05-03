81704f086096cb74e0796a4df4733b2d0bf4115d
jeongjeong2 <wjdgus01227@gmail.com>
2023-05-03 22:09:39 +0900

    23.5.2 velog정리(완)

--- 대학생 기본반 문제집/8979.올림픽.py
+++ 대학생 기본반 문제집/8979.올림픽.py
@@ -6,7 +6,7 @@ country = []
 for i in range(N):
     medal = list(map(int,input().split()))
     country.append(medal)
-country.sort(key = lambda x : (-x[1],-x[2],-x[3])) # 나열 방법 다시 확인
+country.sort(key = lambda x : (-x[1],-x[2],-x[3])) # 나열 방법 다시 확인(check)
 for i in range(N):
     if country[i][0] == K:
         index = i
