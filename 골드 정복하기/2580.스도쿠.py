e662fdc675e8375ef07835697e32da89ba98acf6
Jhyuny <wjdgus01227@gmail.com>
2023-09-29 15:46:57 +0900

    23.9.29

---
+++ 골드 정복하기/2580.스도쿠.py
@@ -0,0 +1,14 @@
+"""
+https://www.acmicpc.net/problem/2580
+"""
+def row(graph,num):
+    """about row"""
+def col(graph,num):
+    """about col"""
+def rect(graph,num):
+    """about rectangular
+    if number in the rectangular continue
+    else push"""
+
+for i in range(1,10):
+    """select number in 1~10"""
