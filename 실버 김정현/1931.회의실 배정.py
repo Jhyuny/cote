4a37c4dbb5db931ef962ee41a79e55a3076979c0
Jhyuny <wjdgus01227@gmail.com>
2023-12-21 17:58:26 +0900

    23.12.21

--- 실버 김정현/1931.회의실 배정.py
+++ 실버 김정현/1931.회의실 배정.py
@@ -7,7 +7,7 @@ https://www.acmicpc.net/problem/1931
 N = int(input())
 dp = [0]*N
 meeting = [list(map(int,input().split())) for _ in range(N)]
-meeting.sort(key=lambda x:[x[1],x[0]]) # x[0]에 대해서는 reverse sort
+meeting.sort(key=lambda x:[x[1],x[0]]) # x[0]에 대해서도 sort ex(1,2) (2,2) 인경우 (2,2)가 먼저 오면 (1,2)는 count x
 
 cnt = 1
 end_t = meeting[0][1]
