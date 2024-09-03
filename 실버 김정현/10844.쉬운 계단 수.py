8a5ddf03506ec44219a53c65d16bce267d7f77a3
Jhyuny <wjdgus01227@gmail.com>
2024-09-03 12:06:56 +0900

    24.9.3

--- 실버 김정현/10844.쉬운 계단 수.py
+++ 실버 김정현/10844.쉬운 계단 수.py
@@ -3,22 +3,17 @@ https://www.acmicpc.net/problem/10844
 """
 N = int(input())
 
-l = 10**(N-1)
-r = 10**N
+dp = [[0]*10 for _ in range(N+1)]
+for i in range(1, 10):
+    dp[1][i] = 1
 
-def check(l):
-    for i in range(1,len(l)):
-        if abs(l[i-1]-l[i]) == 1:
-            pass
+for i in range(2, N+1):
+    for j in range(10):
+        if j == 0:
+            dp[i][j] = dp[i-1][1]
+        elif j == 9:
+            dp[i][j] = dp[i-1][8]
         else:
-            return 0
-    return 1
+            dp[i][j] = dp[i-1][j-1] + dp[i-1][j+1] 
 
-cnt = 0
-
-for i in range(l,r):
-    num = list(map(int,list(str(i))))
-    if check(num):
-        cnt +=1
-
-print(cnt%1000000000)
+print(sum(dp[N]) % 1000000000)
